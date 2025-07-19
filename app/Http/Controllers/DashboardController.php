cba932140186415c7a332f8877541df90c513026
kubokou <kouh27oneone@gmail.com>
2025-07-19 17:48:53 +0000

    Procfile for Heroku

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\SleepRecord;
+use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
@@ -13,8 +14,11 @@ class DashboardController extends Controller
         $user = Auth::user();
 
         $records = $user->sleepRecords()
-                        ->orderBy('date')
-                        ->get(['date', 'duration']); // 必要なデータのみ取得
+                        ->orderByDesc('date')  // 最新順に取得
+                        ->take(7)              // 最新7件
+                        ->get(['date', 'duration'])
+                        ->sortBy('date')       // 昇順に並び替え（グラフ表示用）
+                        ->values();            // インデックスをリセット
 
         return view('dashboard', compact('records'));
     }
