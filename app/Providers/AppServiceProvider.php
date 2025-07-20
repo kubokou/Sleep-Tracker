617c3448a08de174f911442f00191595a2148e9f
kubokou <kouh27oneone@gmail.com>
2025-07-20 03:53:50 +0000

    update package.json

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,8 +17,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+
+    public function boot()
     {
-        //
+        \URL::forceScheme('https'); //追加
     }
+
 }
